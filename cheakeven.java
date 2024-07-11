3abca121794963c47517806849ed65501782e9f0
vickyshreyas <venkeyshreyas09@gmil.com>
2024-07-11 22:12:53 +0530

    my first repository

---
+++ cheakeven.java
@@ -0,0 +1,15 @@
+class Checkeven{
+     public static void main(string args[])
+{
+ int num1=10;
+if(num1%2==0)
+{
+Sysem.out.println("even number")
+}
+else
+{
+System.out.println("odd num")
+}
+}
+}
+
